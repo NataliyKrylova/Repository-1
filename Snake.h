7b7caace957171cd3fd802fa39a4aa6462409624
NataliyKrylova <nataliy_krylova@mail.ru>
2018-06-19 21:24:24 +0300

    Commit 2

--- Snake.h
+++ Snake.h
@@ -23,5 +23,93 @@ return 0;
 return 0;
 }
 
+int colliding(int *snakeArray) {
+//Chek for a collision with the wall
+int x = *snakeArray;
+int y = *(snakeArray+1);
+if (x < 0 || x > MAXWIDTH) {
+return 1;
+}
+else if (y < 0 || y > MAXHEIGHT) {
+return 1;
+}
+
+return 0;
+}
+
+void moveSnake (int *snakeArray, int dirX, int dirY, int speed, int snakeLength) {
+//Moves the snake to one position in the specified direction, also controls the speed play
+int oldX = 0;
+int oldY = 0;
+int curX = *snakeArray;
+int curY = *(snakeArray+1);
+int newX = curX + dirX;
+int newY = curY + dirY;
+
+*(snakeArray) = newX;
+*(snakeArray+1) = newY;
+mvprintw(newY, newX, "S");
+
+for (size_t i = 2; i < snakeLength*2; i += 2) {
+oldX = curX;
+oldY = curY;
+cutX = *(snakeArray+i);
+cutY = *(snakeArray+i+1);
+newX = oldX;
+newY = oldY;
+*(snakeArray+i) = newX;
+*(snakeArray+i+1) = newY;
+mvprintw(newY, newX, "S");
+}
+
+if (dirY != 0) {
+usieep (1000000/speed);
+}
+
+usleep(1000000/speed);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
